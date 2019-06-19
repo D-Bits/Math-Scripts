9b463b0fd969baa0283167c04ca592aeac2b8ae5
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-06-19 00:12:57 -0700

    Added new unit test for dot product.

--- tests/test_dotproduct.py
+++ tests/test_dotproduct.py
@@ -8,4 +8,12 @@ class TestDotProduct(TestCase):
     at right angles to each other is always zero. """   
     def test_right_angle(self):
 
-        self.assertEqual(get_dot_product(90, 15, 18), 0)
+        self.assertAlmostEqual(get_dot_product(90, 15, 18), 0)
+
+    """ Test that the dot product of two vectors at 
+    angles between 90 and 180 degrees are always
+    negative  """
+    def test_90_or_180(self):
+
+        self.assertLess(get_dot_product(120, 12, 20), 0)
+        self.assertLess(get_dot_product(152, 34, 28), 0)
